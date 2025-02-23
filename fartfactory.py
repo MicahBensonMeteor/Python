da508077acac346a64bcac50f08b0ffde6fb6afa
MicahBensonMeteor <micahpianobenson@gmail.com>
2025-02-23 18:54:12 -0500

    Create fartfactory.py

---
+++ fartfactory.py
@@ -0,0 +1,19 @@
+print("Welcome to the stink factory!")
+a=0
+i=0
+hi=8.5
+while a<=5:
+    a+=1
+    print("stink level 1","phhh",a,"not over yet","phhhhhhhhh!",i)
+    i+=2
+    
+while a>=2:
+    a-=1
+    print("going down to",a,"level")
+while i>=2:
+    i-=1
+    print("phhhhhh!!!",i)
+print("now for the last level")    
+while hi>0:
+    hi-=0.5
+    print("toot",hi)
