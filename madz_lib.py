d7c340d19c0c494d3ca62df00b024dc5d4da8a6f
MicahBensonMeteor <micahpianobenson@gmail.com>
2025-02-23 18:46:03 -0500

    Create madz_lib.py
    madz_lib.py

---
+++ madz_lib.py
@@ -0,0 +1,73 @@
+#import random
+mad_lib1="Be careful not to vacuum the"
+madlib1="when you clean under your bed."
+#noun = int(input("pick a number 1-10"))
+noun = input("pick one from this list: \
+        car,\
+        dog,\
+        underwear,\
+        playground,\
+        usa,\
+        earthquake,\
+        money,\
+        cow,\
+        poop,\
+        child,\
+        book\
+        keybord:")
+#noundict={
+#        "book": "car",
+       # "car": "dog",
+        #"usa": "underwear",
+        #"underwear": "playground",
+       # "money": "usa",
+       # "cow": "earthquake",
+      #  "poop": "money",
+     #   "child": "cow",
+    #    "book": "poop",
+   #     "dog":"child",
+  #      "earthquake": "book",
+ #       "keybord:": "car,dog,underwear,playground,usa,earthqua
+ke,money,cow,poop,clild, book"
+#        } 
+#nounlist=["car","dog","underwear","playground","usa","erthquak
+e","money","cow","poop","child","book","keyboard"]
+#print(mad_lib1,noundict[noun],madlib1)
+print(mad_lib1,nounlist[noun-1],madlib1)
+#print(mad_lib1,random.choice(nounlist),madlib1)#import random
+mad_lib1="Be careful not to vacuum the"
+madlib1="when you clean under your bed."
+#noun = int(input("pick a number 1-10"))
+noun = input("pick one from this list: \
+        car,\
+        dog,\
+        underwear,\
+        playground,\
+        usa,\
+        earthquake,\
+        money,\
+        cow,\
+        poop,\
+        child,\
+        book\
+        keybord:")
+#noundict={
+#        "book": "car",
+       # "car": "dog",
+        #"usa": "underwear",
+        #"underwear": "playground",
+       # "money": "usa",
+       # "cow": "earthquake",
+      #  "poop": "money",
+     #   "child": "cow",
+    #    "book": "poop",
+   #     "dog":"child",
+  #      "earthquake": "book",
+ #       "keybord:": "car,dog,underwear,playground,usa,earthqua
+ke,money,cow,poop,clild, book"
+#        } 
+#nounlist=["car","dog","underwear","playground","usa","erthquak
+e","money","cow","poop","child","book","keyboard"]
+#print(mad_lib1,noundict[noun],madlib1)
+print(mad_lib1,nounlist[noun-1],madlib1)
+#print(mad_lib1,random.choice(nounlist),madlib1)
