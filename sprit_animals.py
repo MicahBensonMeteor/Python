3c890e484b536630287cccf78e64cc57e225fea3
MicahBensonMeteor <micahpianobenson@gmail.com>
2025-02-23 18:51:49 -0500

    Create sprit_animals.py
    what's your animal

---
+++ sprit_animals.py
@@ -0,0 +1,33 @@
+import random
+animalsdick={
+        "Unicorn",
+        "Narwale",
+        "Hourse",
+        "Nacked mircat",
+        "dog",
+        "cat",
+        "tortoises",
+        "lion",
+        "bever",
+        "tiger",
+        "cow",
+        "bear",
+        "blobfish",
+        "giraffe",
+        "dinosaur",
+        "wolf",
+        "koala",
+        "red panda",
+        "axolotl",
+        "turtles",
+        "cheetah",
+        "raccoon",
+        "leopard",
+        "shark",
+        "GOAT!!!"
+        "unicorn"
+        "goat"
+        "NARWALE"
+        }
+animalslist=["Unicorn","Narwale","Hourse","Nacked mircat","dog","cat","tortoises","lion","bever","tiger","cow","bear","blobfish","giraffe","dinosaur","wolf","koala","red panda","axolotl","turtles","cheetah","raccoon","leopard","sark","GOAT!!!","unicron","unicorn","unicorn","goat","goat","goat","goat","goat","NARWALE","NARWALE","NARWALE","NARWALE","Houres","Houres","Houres","Hourse","dog","dog","dog","dog","dog","dog","dog","dog","dog","cat","cat","cat","cat","cat","tortoises","tortoises","lion","lion","lion","lion","bever","bever","bever","bever","tiger","tiger","tiger","tieger","cow","cow","cow","cow","cow","cow","cow","bear","bear","bear","bear","giraffe","giraffe","wolf","wolf","wolf","wolf","cheetah","cheetah","cheetah","raccoon"]
+print(b,random.choice(animalslist))
